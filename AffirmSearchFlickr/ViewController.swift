182a61df285f79c013bc23352181bd0492f5bb36
Joe Bradley <bradleyja@gmail.com>
2018-11-12 23:48:52 -0800

    Updates for infinite scrolling for search results

--- AffirmSearchFlickr/ViewController.swift
+++ AffirmSearchFlickr/ViewController.swift
@@ -22,11 +22,14 @@ class ViewController: UIViewController, UISearchBarDelegate, UICollectionViewDel
     let imageCellIdentifier = "imageCellIdentifier"
     let imageViewControllerID = "imageViewControllerID"
     let placeholderImage = "placeholderImage"
+    
+    static let defaultNumberOfResultSets = 2
 
     // TODO: Override imageDataArray setter to store results for search text
     //          to allow for offline search results
     
     var imageDataArray = [AnyObject]()
+    var numberOfResultSets = defaultNumberOfResultSets
     
     // MARK: Life cycle
     
@@ -46,6 +49,9 @@ class ViewController: UIViewController, UISearchBarDelegate, UICollectionViewDel
     // MARK: Search bar delegate
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        // Reset numberOfResultSets
+        numberOfResultSets = ViewController.defaultNumberOfResultSets
+        
         if searchText.trimmingCharacters(in: .illegalCharacters) != "" {
             // Search flickr
             self.searchFlickr(withText: searchText)
@@ -61,9 +67,12 @@ class ViewController: UIViewController, UISearchBarDelegate, UICollectionViewDel
     // MARK: Collection view delegate
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // Deselect cell
         collectionView.cellForItem(at: indexPath)?.isSelected = false
         
-        guard let imageURLPath = self.imageDataArray[indexPath.row]["url_s"] as? String else {
+        let arrayIndex = self.fetchArrayIndex(forIndexPath: indexPath)
+        
+        guard let imageURLPath = self.imageDataArray[arrayIndex]["url_s"] as? String else {
             return
         }
         
@@ -83,14 +92,21 @@ class ViewController: UIViewController, UISearchBarDelegate, UICollectionViewDel
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.imageDataArray.count
+        return self.imageDataArray.count * self.numberOfResultSets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if (self.collectionView.cellForItem(at: indexPath) as? CollectionViewCell) != nil {
+            (self.collectionView.cellForItem(at: indexPath) as! CollectionViewCell).imageView.image = UIImage.init(named: "imagePlaceholder")
+        }
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let imageCell = self.collectionView.dequeueReusableCell(withReuseIdentifier: imageCellIdentifier, for: indexPath) as! CollectionViewCell
+        let arrayIndex = self.fetchArrayIndex(forIndexPath: indexPath)
 
         // Setup url from image data array for index
-        if let imageURLPath = self.imageDataArray[indexPath.row]["url_s"] as? String {
+        if let imageURLPath = self.imageDataArray[arrayIndex]["url_s"] as? String {
             
             // Setup cached image
             let cacheKey: String = imageURLPath
@@ -120,10 +136,37 @@ class ViewController: UIViewController, UISearchBarDelegate, UICollectionViewDel
             imageCell.imageView.image = UIImage.init(named: "imagePlaceholder")
         }
         
+        // Increment number of result sets if near the end of result set (if not already increased)
+        let thresholdRowCount = ((self.imageDataArray.count - 1) * (self.numberOfResultSets - 1))
+        
+        // TODO: Check that number of results sets not already accounted for to avoid incrementing unneccisarily
+        if indexPath.row >= thresholdRowCount {
+            self.numberOfResultSets += 1
+            
+            // TODO: Reload only cells that exist beyond threshold
+            self.collectionView.reloadData()
+        }
+        
         return imageCell as UICollectionViewCell
     }
     
-    // MARK: Custom search
+    // MARK: Custom
+    
+    func fetchArrayIndex(forIndexPath indexPath: IndexPath) -> Int {
+        var arrayIndex = 0
+        
+        // Use number of instances of result set to figure out which index in results array
+        if indexPath.row < self.imageDataArray.count {
+            arrayIndex = indexPath.row
+        }
+            
+        else {
+            // Get the remainder of the modulus of row times instance for array count
+            arrayIndex = (self.numberOfResultSets * indexPath.row) % self.imageDataArray.count
+        }
+        
+        return arrayIndex
+    }
 
     func fetchSearchURLPath(withText searchText: String) -> String {
         return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=675894853ae8ec6c242fa4c077bcf4a0&text=\(searchText)&extras=url_s&format=json&nojsoncallback=1"
