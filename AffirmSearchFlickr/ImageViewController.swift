814c3876d3d50e837587064945e2efc9ed8b9160
Joe Bradley <bradleyja@gmail.com>
2018-11-12 23:24:41 -0800

    - Setup basic image search and display for main view controller and detail view view, along with adding third party libraries (using cocoa pods) for downloading data and cacheing images

--- AffirmSearchFlickr/ImageViewController.swift
+++ AffirmSearchFlickr/ImageViewController.swift
@@ -7,25 +7,60 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ImageViewController: UIViewController {
 
+    // MARK: IB outlet vars
+
     @IBOutlet weak var imageView: UIImageView!
     
+    // MARK: Constants and vars
+    
+    let placeholderImage = "placeholderImage"
+
+    var imageViewURLPath :String?
+    
+    // MARK: Life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.imageView.image = UIImage.init(named: "imagePlaceholder")
+        
+        if self.imageViewURLPath != nil {
+            
+            // NOTE: Copied cached code from ViewController
+            // TODO: Create a shared function setting up cached image
+            
+            // Setup cached image
+            let cacheKey: String = self.imageViewURLPath!
+            var cachedImage = SDImageCache.shared().imageFromDiskCache(forKey: cacheKey)
+            
+            if cachedImage == nil {
+                cachedImage = SDImageCache.shared().imageFromMemoryCache(forKey: cacheKey)
+            }
+            
+            if cachedImage != nil {
+                // Use cached image
+                self.imageView.image = cachedImage
+            }
+                
+            else {
+                // Setup image for url
+                let imageURL = URL.init(string: self.imageViewURLPath!)
+                
+                self.imageView.sd_setImage(with: imageURL,
+                                           placeholderImage: UIImage.init(named: placeholderImage),
+                                           options: SDWebImageOptions(),
+                                           completed: nil)
+            }
+        }
+        
+        else {
+            self.imageView.image = UIImage.init(named: "imagePlaceholder")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-
-    // MARK: - Navigation
-
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        self.imageView.image = UIImage.init(named: "imagePlaceholder")
-//    }
 }
--- AffirmSearchFlickr/ViewController.swift
+++ AffirmSearchFlickr/ViewController.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import AFNetworking
+import SDWebImage
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class ViewController: UIViewController, UISearchBarDelegate, UICollectionViewDelegate, UICollectionViewDataSource {
 
     // MARK: IB outlet vars
 
@@ -18,25 +20,60 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     // MARK: Constants and vars
     
     let imageCellIdentifier = "imageCellIdentifier"
-    let imageViewSegue = "imageViewSegue"
+    let imageViewControllerID = "imageViewControllerID"
+    let placeholderImage = "placeholderImage"
+
+    // TODO: Override imageDataArray setter to store results for search text
+    //          to allow for offline search results
+    
+    var imageDataArray = [AnyObject]()
     
     // MARK: Life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.collectionView.allowsSelection = true
+        // TODO: Check if offline and display message
+        // TODO: Show recent searches that match cached results
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
+        // TODO: Clear out cached images and/or data
+    }
+    
+    // MARK: Search bar delegate
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.trimmingCharacters(in: .illegalCharacters) != "" {
+            // Search flickr
+            self.searchFlickr(withText: searchText)
+        }
+        
+        else {
+            // Reset search results
+            self.imageDataArray = [AnyObject]()
+            self.collectionView.reloadData()
+        }
     }
 
     // MARK: Collection view delegate
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.cellForItem(at: indexPath)?.isSelected = false
-        self.performSegue(withIdentifier: imageViewSegue, sender: nil)
+        
+        guard let imageURLPath = self.imageDataArray[indexPath.row]["url_s"] as? String else {
+            return
+        }
+        
+        let imageViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: self.imageViewControllerID)  as! ImageViewController
+
+        // Set image url path
+        imageViewController.imageViewURLPath = imageURLPath
+
+        // Push image view controller
+        self.navigationController?.pushViewController(imageViewController, animated: true)
     }
     
     // MARK: Collection view data source
@@ -46,17 +83,97 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return self.imageDataArray.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let imageCell = self.collectionView.dequeueReusableCell(withReuseIdentifier: imageCellIdentifier, for: indexPath) as! CollectionViewCell
 
-        imageCell.imageView.image = UIImage.init(named: "imagePlaceholder")
+        // Setup url from image data array for index
+        if let imageURLPath = self.imageDataArray[indexPath.row]["url_s"] as? String {
+            
+            // Setup cached image
+            let cacheKey: String = imageURLPath
+            var cachedImage = SDImageCache.shared().imageFromDiskCache(forKey: cacheKey)
+            
+            if cachedImage == nil {
+                cachedImage = SDImageCache.shared().imageFromMemoryCache(forKey: cacheKey)
+            }
+            
+            if cachedImage != nil {
+                // Use cached image
+                imageCell.imageView.image = cachedImage
+            }
+
+            else {
+                // Setup image for url
+                let imageURL = URL.init(string: imageURLPath)
+                
+                imageCell.imageView.sd_setImage(with: imageURL,
+                                                placeholderImage: UIImage.init(named: placeholderImage),
+                                                options: SDWebImageOptions(),
+                                                completed: nil)
+            }
+        }
+        
+        else {
+            imageCell.imageView.image = UIImage.init(named: "imagePlaceholder")
+        }
         
         return imageCell as UICollectionViewCell
     }
     
+    // MARK: Custom search
 
+    func fetchSearchURLPath(withText searchText: String) -> String {
+        return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=675894853ae8ec6c242fa4c077bcf4a0&text=\(searchText)&extras=url_s&format=json&nojsoncallback=1"
+    }
+    
+    func searchFlickr(withText searchText: String) {
+        let manager = AFHTTPSessionManager()
+        let urlPath = self.fetchSearchURLPath(withText: searchText)
+        
+        //print("response = \(String(describing: response))")
+
+        // TODO: Display activity indicator
+        
+        manager.responseSerializer = AFJSONResponseSerializer()
+        
+        manager.get(urlPath, parameters: nil, progress: nil, success: { (task, response) in
+            guard let responseDictionary = response as? [String: AnyObject] else {
+                self.imageDataArray = [AnyObject]()
+                return
+            }
+            guard let photosDictionary = responseDictionary["photos"] as? [String: AnyObject] else {
+                self.imageDataArray = [AnyObject]()
+                return
+            }
+            guard let photosArray = photosDictionary["photo"] as? [AnyObject] else {
+                self.imageDataArray = [AnyObject]()
+                return
+            }
+            
+            print("count = \(photosArray.count)")
+
+            // Set image data array
+            self.imageDataArray = photosArray
+
+            // TODO: Hide activity indicator
+            
+            // Reload collection view for new data
+            self.collectionView.reloadData()
+
+        }) { (task, error) in
+            // Reset image data array
+            self.imageDataArray = [AnyObject]()
+
+            // TODO: Display error to user
+            // TODO: Hide activity indicator
+            print("error = \(error)")
+            
+            // Reload collection view for empty data
+            self.collectionView.reloadData()
+        }
+    }
 }
 
